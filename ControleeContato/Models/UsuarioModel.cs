3e1fc6333a5bfcdb651a6a11f6df6b57908b8b20
David <david_contardi@live.com>
2023-01-08 14:30:34 -0300

    Atualização

--- ControleeContato/Models/UsuarioModel.cs
+++ ControleeContato/Models/UsuarioModel.cs
@@ -1,4 +1,5 @@
 ﻿using ControleeContato.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleeContato.Models
 {
@@ -7,14 +8,19 @@ namespace ControleeContato.Models
         public int Id { get; set; }
 
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o nome do usuário")]
 
         public string Login { get; set; }
+        [Required(ErrorMessage = "Digite o login do usuário")]
 
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail do usuário")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é valido!")]
 
         public PerfilEnum Perfil { get; set; }
 
         public string Senha { get; set; }
+        [Required(ErrorMessage = "Digite a senha do usuário")]
 
         public DateTime DataCadastro { get; set; }
 
